684eb9d2c8b5f7e412624c6a2440fddab6d759e0
Ilya Derevyashkin <iderevyashkin@kameleoon.com>
2021-11-21 11:57:53 +1000

    adaptive

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  isActive: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  toggleSidebar() {
+    this.isActive = !this.isActive;
   }
 
+  ngOnInit(): void {}
 }
